bcd17ba9907a430345bce7e47f27c45cce2c4dbe
Lajcee Rakete-Miller <270026313@bun2-206-03.acg.ac.nz>
2021-03-02 16:59:43 +1300

    slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 console.log('json');
 
       $(window).load(function() {
         $('.flexslider').flexslider();
       });
+=======
+$(document).ready(function(){
+  $(window).load(function() {
+    $('.flexslider').flexslider();
+  });
+
+})
+>>>>>>> c3073fefc8162f4fbee310b68c8e7d426999b9d8
